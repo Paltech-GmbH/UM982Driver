f5c0d96c9510c5ebd3fcc0d37367d48ab278fed0
Umar-Senpai <muhammadumar559@gmail.com>
2025-05-06 14:10:16 +0200

    remove ntrip connection

--- UM982Driver/um982/UM982.py
+++ UM982Driver/um982/UM982.py
@@ -20,6 +20,7 @@ def crc_table():
         table.append(crc)
     return table
 
+
 NMEA_EXPEND_CRC_TABLE = crc_table()
 
 
@@ -36,7 +37,8 @@ def nmea_expend_crc(nmea_expend_sentence):
     except:
         return False
     calculated_crc = calculate_crc32(sentence.encode())
-    return crc.lower() == format(calculated_crc, '08x')
+    return crc.lower() == format(calculated_crc, "08x")
+
 
 def nmea_crc(nmea_sentence):
     # 移除起始的'$'和'*'及之后的校验和部分
@@ -50,55 +52,69 @@ def nmea_crc(nmea_sentence):
     for char in sentence:
         calculated_checksum ^= ord(char)
     # 将计算得到的校验和转换为十六进制格式，并大写
-    calculated_checksum_hex = format(calculated_checksum, 'X')
+    calculated_checksum_hex = format(calculated_checksum, "X")
     # 校验和比较
     return calculated_checksum_hex.zfill(2) == crc.upper()
 
-def msg_seperate(msg:str):
-    return msg[1:msg.find('*')].split(',')
 
-def PVTSLN_solver(msg:str):
+def msg_seperate(msg: str):
+    return msg[1 : msg.find("*")].split(",")
+
+
+def PVTSLN_solver(msg: str):
     parts = msg_seperate(msg)
-    bestpos_hgt    = float(parts[3+7])          # 海拔高，单位：米
-    bestpos_lat    = float(parts[4+7])          # 纬度，单位：度 （输出小数点后 11 位）
-    bestpos_lon    = float(parts[5+7])          # 经度，单位：度 （输出小数点后 11 位）
-    bestpos_hgtstd = float(parts[6+7])          # 高程标准差，单位：米
-    bestpos_latstd = float(parts[7+7])          # 纬度标准差，单位：米
-    bestpos_lonstd = float(parts[8+7])          # 经度标准差，单位：米
-    fix = (bestpos_hgt, bestpos_lat, bestpos_lon, bestpos_hgtstd, bestpos_latstd, bestpos_lonstd)
+    bestpos_hgt = float(parts[3 + 7])  # 海拔高，单位：米
+    bestpos_lat = float(parts[4 + 7])  # 纬度，单位：度 （输出小数点后 11 位）
+    bestpos_lon = float(parts[5 + 7])  # 经度，单位：度 （输出小数点后 11 位）
+    bestpos_hgtstd = float(parts[6 + 7])  # 高程标准差，单位：米
+    bestpos_latstd = float(parts[7 + 7])  # 纬度标准差，单位：米
+    bestpos_lonstd = float(parts[8 + 7])  # 经度标准差，单位：米
+    fix = (
+        bestpos_hgt,
+        bestpos_lat,
+        bestpos_lon,
+        bestpos_hgtstd,
+        bestpos_latstd,
+        bestpos_lonstd,
+    )
     return fix
 
-def GNGGA_solver(msg:str):
+
+def GNGGA_solver(msg: str):
     parts = msg_seperate(msg)
-    quality = int(parts[7-1])
+    quality = int(parts[7 - 1])
     return quality
 
-def GNHPR_solver(msg:str):
+
+def GNHPR_solver(msg: str):
     parts = msg_seperate(msg)
-    heading = float(parts[3-1])
-    pitch   = float(parts[4-1])
-    roll    = float(parts[5-1])
+    heading = float(parts[3 - 1])
+    pitch = float(parts[4 - 1])
+    roll = float(parts[5 - 1])
     orientation = (heading, pitch, roll)
     return orientation
 
-def UNIHEADING_solver(msg:str):
+
+def UNIHEADING_solver(msg: str):
     parts = msg_seperate(msg)
-    heading = float(parts[5+7])
-    hdgstddev = float(parts[8+7])
+    heading = float(parts[5 + 7])
+    hdgstddev = float(parts[8 + 7])
     uniheading_msg = (heading, hdgstddev)
     return uniheading_msg
 
-def BESTNAV_solver(msg:str):
+
+def BESTNAV_solver(msg: str):
     parts = msg_seperate(msg)
     vel_hor_std = float(parts[-1])  # 水平速度标准差，单位 m/s
     vel_ver_std = float(parts[-2])  # 高程速度标准差，单位 m/s
-    vel_ver     = float(parts[-3])  # 垂直速度， m/s，正值表示高度增加（向上），负值表示高度下降（向下）
+    vel_ver = float(
+        parts[-3]
+    )  # 垂直速度， m/s，正值表示高度增加（向上），负值表示高度下降（向下）
     vel_heading = float(parts[-4])  # 相对于真北的实际对地运动方向（相对地面轨迹）， deg
-    vel_hor     = float(parts[-5])  # 对地水平速度， m/s
-    vel_north   = vel_hor * math.cos(math.radians(vel_heading))     # 分解得到北方向速度
-    vel_east    = vel_hor * math.sin(math.radians(vel_heading))     # 分解得到东方向速度
+    vel_hor = float(parts[-5])  # 对地水平速度， m/s
+    vel_north = vel_hor * math.cos(math.radians(vel_heading))  # 分解得到北方向速度
+    vel_east = vel_hor * math.sin(math.radians(vel_heading))  # 分解得到东方向速度
     return (vel_east, vel_north, vel_ver, vel_hor_std, vel_hor_std, vel_ver_std)
-
 
 
 def create_utm_trans(lat, lon):
@@ -112,16 +128,16 @@ def create_utm_trans(lat, lon):
         transformer: 转换器
     """
     # UTM区号是根据经度确定的，从-180度开始每6度一个区间。
-    zone_number            = int((lon + 180) / 6) + 1
+    zone_number = int((lon + 180) / 6) + 1
     # 北半球是赤道（纬度0度）以上的区域。
-    isnorth                = lat >= 0
+    isnorth = lat >= 0
     # 定义WGS84坐标系
-    wgs84_crs              = CRS("epsg:4326")
+    wgs84_crs = CRS("epsg:4326")
     # 根据是否位于北半球，选择合适的UTM EPSG代码
-    utm_crs_str            = f"epsg:326{zone_number}" if isnorth else f"epsg:327{zone_number}"
-    utm_crs                = CRS(utm_crs_str)
+    utm_crs_str = f"epsg:326{zone_number}" if isnorth else f"epsg:327{zone_number}"
+    utm_crs = CRS(utm_crs_str)
     # 创建坐标转换器，从WGS84转换到UTM
-    transformer            = Transformer.from_crs(wgs84_crs, utm_crs, always_xy=True)
+    transformer = Transformer.from_crs(wgs84_crs, utm_crs, always_xy=True)
     return transformer
 
 
@@ -137,75 +153,37 @@ def utm_trans(transformer, lon, lat):
         tuple: 一个元组，包含转换后的UTM坐标系下的x（东坐标）和y（北坐标）。
     """
     # 进行坐标转换
-    utm_x, utm_y           = transformer.transform(lon, lat)
+    utm_x, utm_y = transformer.transform(lon, lat)
     return (utm_x, utm_y)
 
-def string_to_base64(input_string):
-    input_bytes = input_string.encode("utf-8")
-    base64_encoded = base64.b64encode(input_bytes)
-    return base64_encoded.decode("utf-8")
 
-def connect_ntrip(host, port, mountpoint, username, password):
-    """Connect to NTRIP caster and stream RTCM data."""
-    auth = string_to_base64(f"{username}:{password}")
-    authorization = f"Authorization: Basic {auth}\r\n"
-    url = f"/{mountpoint}\r\n"
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host, port))
-    request = f"GET {url}HTTP/1.1\r\nUser-Agent: NTRIP GNSSInternetRadio/1.4.10\r\nAccept: */*\r\nConnection: close\r\n{authorization}\r\n"
-    sock.sendall(request.encode())
-
-    response = sock.recv(1024)
-
-    if b"ICY 200 OK" not in response:
-        raise ConnectionError("Failed to connect to NTRIP caster")
-    return sock
-
-
-class UM982Serial():
-    def __init__(self, port, band, ntrip_host, ntrip_port, mountpoint, username, password):
+class UM982Serial:
+    def __init__(self, port, band):
         super().__init__()
         # 打开串口
-        self.ser            = serial.Serial(port, band)
+        self.ser = serial.Serial(port, band)
         # 设置运行标志位
-        self.isRUN          = True
+        self.isRUN = True
         # 数据
-        self.fix            = None   # sensor_msgs/NavSatFix 所需要的数据
-        self.orientation    = None   # 航向角
-        self.vel            = None   # 速度
-        self.utmpos         = None
-        self.uniheading     = None
-        self.ntrip_socket   = None
-        self.last_gga       = None
-        self.rtk_status     = 0
+        self.fix = None  # sensor_msgs/NavSatFix 所需要的数据
+        self.orientation = None  # 航向角
+        self.vel = None  # 速度
+        self.utmpos = None
+        self.uniheading = None
+        self.last_gga = None
+        self.rtk_status = 0
         # 读初始数据
-        try:
-            self.ntrip_socket = connect_ntrip(ntrip_host, ntrip_port, mountpoint, username, password)
-            print("Connected to NTRIP Caster.")
-        except Exception as e:
-            print(f"Error: {e}")
-            return
-        for i in range(10):
+        for _ in range(10):
             self.read_frame()
 
-    def forward_to_serial(self, sock, gga_message):
-        """Read RTCM data from NTRIP and forward to serial port."""
-        # with serial.Serial(SERIAL_PORT, BAUDRATE, timeout=1) as ser:
-        sock.sendall(gga_message.encode())
+    def forward_to_serial(self, message):
         try:
-            data, _ = sock.recvfrom(1024)
-            if data:
-                try:
-                    rtcm_message = RTCMMessage(data)
-                    if rtcm_message:
-                        self.ser.write(data)
-                except Exception as e:
-                    print(f"Error parsing RTCM: {e}")
+            self.ser.write(message)
         except Exception as e:
-            print(f"Error Receiving DATA: {e}")
+            print(f"Error Writing RTCM message: {e}")
 
     def read_frame(self):
-        frame = self.ser.readline().decode('utf-8')
+        frame = self.ser.readline().decode("utf-8")
         if frame.startswith("$GNGGA") and nmea_crc(frame):
             self.last_gga = frame
             self.rtk_status = GNGGA_solver(frame)
@@ -217,13 +195,11 @@ class UM982Serial():
             self.vel = BESTNAV_solver(frame)
         elif frame.startswith("#UNIHEADINGA") and nmea_expend_crc(frame):
             self.uniheading = UNIHEADING_solver(frame)
-    
-    def send_rtcm(self):
-        if self.ntrip_socket is not None and self.last_gga is not None:
-            self.forward_to_serial(self.ntrip_socket, self.last_gga)
+
+    def send_rtcm(self, message):
+        self.forward_to_serial(message)
 
 
 if __name__ == "__main__":
     um982 = UM982Serial("/dev/rtk1", 921600)
     um982.start()
-
